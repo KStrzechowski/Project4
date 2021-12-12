e4575e6386028036aa8eecc407b6e1a9e73faa26
KStrzechowski <strzechowski.konrad@gmail.com>
2021-12-13 00:27:08 +0100

    Adding IGraphicObjcet interface and static class Helpful.

--- GraphicObjects/IGraphicObject.cs
+++ GraphicObjects/IGraphicObject.cs
@@ -10,9 +10,19 @@ namespace Project4.GraphicObjects
     public interface IGraphicObject
     {
         void Draw();
-        void Select();
-        void UnSelect();
+        bool Selected { get; set; }
+        Graphics Graphic { get; set; }
         public void Move(Point startingPoint, Point endingPoint);
         public bool CheckIfClicked(Point point);
+    }
+
+    public static class Helpful
+    {
+        public static double GetDistance(Point first, Point second) => 
+            Math.Floor((Math.Sqrt(Math.Pow(first.X - second.X, 2) +
+            Math.Pow(first.Y - second.Y, 2))));
+
+        public static void DrawLine(Graphics graphics, Color color, Point first, Point second) =>
+            graphics.DrawLine(new Pen(new SolidBrush(color)), first, second);
     }
 }
